bcc3385580034b1a62c4b76334e011df6ae8a552
Suraj Kamble <skamble@phoenixgroup.tech>
2023-10-27 11:40:39 +0530

    changes in UI

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,32 +1,37 @@
-import { Offcanvas, Stack } from "react-bootstrap"
-import { useShoppingCart } from "../context/ShoppingCartContext"
-import CartItem from "./CartItem";
+import { Offcanvas, Stack } from 'react-bootstrap';
+import { useShoppingCart } from '../context/ShoppingCartContext';
+import CartItem from './CartItem';
 import storeItems from '../data/items.json';
-import { BsCurrencyRupee } from "react-icons/bs";
+import { BsCurrencyRupee } from 'react-icons/bs';
 
 type ShoppingCartProps = {
-    isOpen : boolean
-}
+  isOpen: boolean;
+};
 
 export default function ShoppingCart({ isOpen }: ShoppingCartProps) {
-    const { closeCart , cartItems } = useShoppingCart();
+  const { closeCart, cartItems } = useShoppingCart();
   return (
-    <Offcanvas show={isOpen} onHide={ closeCart } placement="end">
-        <Offcanvas.Header closeButton>
-            <Offcanvas.Title>Cart</Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body>
-            <Stack gap={3}>
-                { cartItems.map(item => (
-                    <CartItem key={item.id} {...item}/>
-                )) }
-                <div className="ms-auto fw-bold fs-5">Total { cartItems.reduce((total, cartItem) => {
-                    const item = storeItems.find(i => i.id === cartItem.id)
-                    return total + (item?.price || 0 ) * cartItem.quantity 
-                },0) 
-                } <BsCurrencyRupee className='mb-1' /></div>
-            </Stack>
-        </Offcanvas.Body>
+    <Offcanvas show={isOpen} onHide={closeCart} placement='end'>
+      <Offcanvas.Header closeButton>
+        <Offcanvas.Title>Cart</Offcanvas.Title>
+      </Offcanvas.Header>
+      <Offcanvas.Body>
+        {cartItems.length ? (
+          <Stack gap={3}>
+            {cartItems.map((item) => (
+              <CartItem key={item.id} {...item} />
+            ))}
+            <div className='ms-auto fw-bold fs-5'>
+              Total{' '}
+              {cartItems.reduce((total, cartItem) => {
+                const item = storeItems.find((i) => i.id === cartItem.id);
+                return total + (item?.price || 0) * cartItem.quantity;
+              }, 0)}{' '}
+              <BsCurrencyRupee className='mb-1' />
+            </div>
+          </Stack>
+        ) : <span>No Items added in cart</span>}
+      </Offcanvas.Body>
     </Offcanvas>
-  )
+  );
 }
